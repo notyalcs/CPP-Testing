e7f77571fff361d7e8c3cab3aded9de69be8e15c
Sean Clayton <sean.clayto@gmail.com>
2020-11-16 16:49:26 -0800

    FirstCommit

---
+++ helloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string test = "Hello";
+    int x = test.length();
+
+    cout << "Hello World!";
+
+    cout << "\nThis is a test of length:\t" << x;
+
+    return 0;
+}
